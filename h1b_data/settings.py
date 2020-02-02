e3e1384885f635fc4596ff0d7366ec5a9b8149fa
Duncan Evans <duncanevans@Duncans-MacBook-Pro.local>
2020-02-02 14:20:38 -0500

    updates

--- h1b_data/settings.py
+++ h1b_data/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_plotly_dash.middleware.BaseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'h1b_data.urls'
