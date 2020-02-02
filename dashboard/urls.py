31c60ed8d438c9c295e66945a99f7ff720e42f02
Duncan Evans <duncanevans@Duncans-MacBook-Pro.local>
2020-02-02 13:49:14 -0500

    updats

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,7 +3,7 @@ from . import views
 from dashboard.dash_apps.finished_apps import h1b_salary
 
 urlpatterns = [
-    path('', views.h1b_salary_dashboard, name='dashboards_home'),
+    path('', views.h1b_salary_dashboard, name='h1b_salary_dashboard'),
     path('about/', views.about, name='dashboards_about'),
     path('salaries/', views.h1b_salary_dashboard, name='h1b_salary_dashboard'),
 
