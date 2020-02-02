1cd8812d3461bd86945c3d0e9b0aa8da9cd0cc29
Duncan Evans <duncanevans@Duncans-MacBook-Pro.local>
2020-02-02 13:53:20 -0500

    updates

--- h1b_data/settings.py
+++ h1b_data/settings.py
@@ -156,6 +156,7 @@ PLOTLY_COMPONENTS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
